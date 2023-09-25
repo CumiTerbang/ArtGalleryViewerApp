2f59e5b8b1b529890439395a6812e0fa587930d4
CumiTerbang <gitarringsek@gmail.com>
2023-09-25 12:32:00 +0700

    prepare layout

--- app/src/main/java/com/haryop/artgalleryviewerapp/screen/MainActivity.kt
+++ app/src/main/java/com/haryop/artgalleryviewerapp/screen/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.haryop.artgalleryviewerapp
+package com.haryop.artgalleryviewerapp.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haryop.artgalleryviewerapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
